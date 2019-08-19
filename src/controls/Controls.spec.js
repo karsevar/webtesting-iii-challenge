45c29a37e9ce48e389ce6914c7468c8b1176ad3c
masonkarsevar <masonkarsevar@gmail.com>
2019-08-19 13:29:44 -0700

    toggleClosed functionality disabled when locked is true test successful

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -37,5 +37,59 @@ describe('<Controls />', () => {
         fireEvent.click(getByText('Unlock Gate'));
         
         expect(mockLocked).toHaveBeenCalled();
+    });
+
+    it('should display "Open Gate" and "Unlock Gate" when closed and locked are true', () => {
+        const closed = true;
+        const locked = true;
+        const {queryByText} = render(<Controls 
+                                    locked={locked}
+                                    closed={closed}
+                                    />)
+        expect(queryByText('Open Gate')).toBeTruthy();
+        expect(queryByText('Unlock Gate')).toBeTruthy();
+        
+    })
+
+    it('should display "Close Gate" and "Lock Gate" when closed and locked are false', () => {
+        const closed = false;
+        const locked = false;
+        const {queryByText} = render(<Controls 
+                                    locked={locked}
+                                    closed={closed}
+                                    />)
+        expect(queryByText('Close Gate')).toBeTruthy();
+        expect(queryByText('Lock Gate')).toBeTruthy();
+        
+    })
+
+    it('locked button should be disabled when close is false', () => {
+        const closed = false;
+        const locked = false;
+        const lockedMock = jest.fn();
+
+        const {getByText} = render(<Controls
+                                    locked={locked}
+                                    closed={closed} 
+                                    toggleLocked={lockedMock} 
+                                    />)
+        fireEvent.click(getByText('Lock Gate'));
+        
+        expect(lockedMock).not.toHaveBeenCalled();
+    })
+
+    it('Open Gate button should be disabled when locked is true', () => {
+        const closed = true;
+        const locked = true;
+        const closedMock = jest.fn();
+
+        const {getByText} = render(<Controls
+                                    locked={locked}
+                                    closed={closed} 
+                                    toggleClosed={closedMock} 
+                                    />)
+        fireEvent.click(getByText('Open Gate'));
+        
+        expect(closedMock).not.toHaveBeenCalled();
     })
 })
