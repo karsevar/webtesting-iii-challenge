e4eeedc91590d7633081e3e9c115d06ff0ed52f5
masonkarsevar <masonkarsevar@gmail.com>
2019-08-19 12:31:04 -0700

    Dashboard tests are all finished. small bug with the react packages

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,21 @@
-// Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {render} from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+    it('<Dashboard /> renders without crashing', () => {
+        render(<Dashboard />)
+    });
+
+    it('matches snapshot', () => {
+        const tree = renderer.create(<Dashboard />); // generates a DOM tree
+    
+        // snapshots are a JSON representation of the DOM tree
+        expect(tree.toJSON()).toMatchSnapshot();
+    });
+})
+
+// According to snapshot the Display and controls components 
+// all render to Dashboard.
