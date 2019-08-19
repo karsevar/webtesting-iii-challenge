3171ca9f4cb29b9ed362f70c2c3fd719c4105445
masonkarsevar <masonkarsevar@gmail.com>
2019-08-19 08:25:12 -0700

    Display component test render test

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,10 @@
-// Test away!
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Display from './Display';
+import {render, fireEvent} from '@testing-library/react';
+
+describe('<Display /> Components tests', () => {
+    it('renders without crashing', () => {
+        render(<Display />)
+    })
+})
