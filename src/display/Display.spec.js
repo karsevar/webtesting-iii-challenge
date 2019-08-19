5a5b97aaaa20fffbeeb416d181dfb9c9eef29660
masonkarsevar <masonkarsevar@gmail.com>
2019-08-19 08:44:50 -0700

    display tests continued

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -7,4 +7,24 @@ describe('<Display /> Components tests', () => {
     it('renders without crashing', () => {
         render(<Display />)
     })
+
+    it('should display closed if the closed prop is true', async () => {
+        const {getByText, findByText} = render(<Display closed={true} locked={true} />)
+        expect(await findByText(/closed/i))
+    })
+
+    it('should display lock if the lock prop is true', async () => {
+        const {getByTest, findByText} = render(<Display closed={true} locked={true} />) 
+        expect(await findByText(/locked/i)) 
+    })
+
+    it('should display open if the closed prop is false', async () => {
+        const {getByText, findByText} = render(<Display closed={false} locked={true} />)
+        expect(await findByText(/open/i))
+    })
+
+    it('should display unlocked if the locked prop is false', async () => {
+        const {findByText} = render(<Display closed={false} locked={false} />) 
+        expect(await findByText(/unlocked/i))
+    })
 })
